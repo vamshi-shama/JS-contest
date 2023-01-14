aa2e3a40673b6170a43f3633e9e2c8343aabea3f
vamshi <vamshis2017@gmail.com>
2023-01-15 00:08:36 +0530

    .

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const details = document.getElementById("details");
+details.addEventListener('click', getDetails)
+function getDetails() {
+    const latitude = data.latitude;
+    const longitude = data.longitude;
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: latitude, lng: longitude },
+        zoom: 8
+    });
+    const marker = new google.maps.Marker({
+        position: { lat: latitude, lng: longitude },
+        map: map
+    });
+
+}
